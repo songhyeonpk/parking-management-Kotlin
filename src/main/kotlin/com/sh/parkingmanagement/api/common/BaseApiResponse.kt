030f2c30b975df5eebae2000959968b618bd2b7d
songhyeonpk <ehftozl1997@gmail.com>
2024-10-28 17:19:28 +0900

    feat: 일관된 api 응답을 위한 BaseApiResponse 클래스 구현

---
+++ src/main/kotlin/com/sh/parkingmanagement/api/common/BaseApiResponse.kt
@@ -0,0 +1,44 @@
+package com.sh.parkingmanagement.api.common
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+data class BaseApiResponse<D>(
+    val message: String,
+    val data: D? = null
+) {
+    companion object {
+
+        // 성공 응답 (응답 데이터가 없는 경우)
+        fun success(successResponse: SuccessResponse): ResponseEntity<BaseApiResponse<Nothing?>> {
+            return ResponseEntity(
+                BaseApiResponse(successResponse.message),
+                successResponse.status
+            )
+        }
+
+        // 성공 응답 (응답 데이터가 있는 경우)
+        fun <D> success(successResponse: SuccessResponse, data: D): ResponseEntity<BaseApiResponse<D>> {
+            return ResponseEntity(
+                BaseApiResponse(successResponse.message, data),
+                successResponse.status
+            )
+        }
+
+        // 실패, 에러 응답 (응답 데이터가 없는 경우)
+        fun error(status: HttpStatus, message: String): ResponseEntity<BaseApiResponse<Nothing?>> {
+            return ResponseEntity(
+                BaseApiResponse(message),
+                status
+            )
+        }
+
+        // 실패, 에러 응답 (응답 데이터가 있는 경우)
+        fun <D> error(status: HttpStatus, message: String, data: D): ResponseEntity<BaseApiResponse<D>> {
+            return ResponseEntity(
+                BaseApiResponse(message, data),
+                status
+            )
+        }
+    }
+}
