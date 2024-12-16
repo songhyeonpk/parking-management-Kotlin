a611183f4ead94dd520d2b1d27a48297d8485584
songhyeonpk <ehftozl1997@gmail.com>
2024-12-16 16:11:01 +0900

    feat: 주차 이벤트 관련 API 목록을 관리하는 ParkingEventsApiSpec 추가

---
+++ src/main/kotlin/com/sh/parkingmanagement/api/docs/ParkingEventApiSpec.kt
@@ -0,0 +1,7 @@
+package com.sh.parkingmanagement.api.docs
+
+import io.swagger.v3.oas.annotations.tags.Tag
+
+@Tag(name = "parking events", description = "주차이벤트 API")
+interface ParkingEventApiSpec {
+}
